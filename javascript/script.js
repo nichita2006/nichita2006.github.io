e0be2f1584c1085b4daeb1bc8277a6cf2ec6a6fd
nichita2006 <161025778+nichita2006@users.noreply.github.com>
2024-03-13 21:32:40 +0200

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,41 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const smoothScrollLinks = document.querySelectorAll('.footer-area a[href^="#"]');
+  
+    for (const link of smoothScrollLinks) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+  
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+  
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    }
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+window.addEventListener('load', function () {
+    document.getElementById('footer-map').classList.add('fadeIn');
+});
+
+
+function toggleMenu() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = (navLinks.style.display === 'none' || navLinks.style.display === '') ? 'block' : 'none';
+}
